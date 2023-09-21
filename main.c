f4f22d25dde618495f07a5587db2f170c45c7e12
Chamomile <dltldms5271@gmail.com>
2023-09-21 16:43:39 +0900

    4

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    char c;
+    int a;
+    int b; 
     
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요: ");
+    scanf("%d", &a);
+    printf("분모를 입력하세요: ");
+    scanf("%d", &b);
     
-    printf("the next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+    printf("나누기의 결과는 %f 입니다.\n", (float)a/b);
   
   system("PAUSE");	
   return 0;
